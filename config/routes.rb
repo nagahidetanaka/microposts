7dd79095f2afb1012fd6694ab7dbe7de253ed574
nagahidetanaka <tnag101010@ezweb.ne.jp>
2016-11-11 04:37:19 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   get 'signup', to: 'users#new'
   
-  resources :users
+  resources :microposts
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
