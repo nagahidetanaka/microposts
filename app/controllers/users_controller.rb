94f9923e762669e46cb752501202529664cc8512
nagahidetanaka <tnag101010@ezweb.ne.jp>
2016-11-11 06:11:51 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   
-  def show # 追加
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc) # 追加
   end
     
   def new
